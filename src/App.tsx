b44bb28117db060d5e7a37be5e65f67f34eefdb8
Oleg Prilepa <o.a.prilepa@gmail.com>
2024-05-28 23:06:04 +0700

    dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
-import { Container, Grid } from '@mui/material';
-import { FC } from 'react';
+import {
+  Container,
+  createTheme,
+  CssBaseline,
+  Grid,
+  ThemeProvider,
+  useMediaQuery,
+} from '@mui/material';
+import React, { FC } from 'react';
 
-const App: FC = () => (
-  <Container disableGutters>
-    <Grid container spacing={2}>
-      <Grid item xs={4}>
-        TODO: Task list
-      </Grid>
-      <Grid item xs={8}>
-        TODO: Gantt chart
-      </Grid>
-    </Grid>
-  </Container>
-);
+const App: FC = () => {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode]
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container disableGutters>
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            TODO: Task list
+          </Grid>
+          <Grid item xs={8}>
+            TODO: Gantt chart
+          </Grid>
+        </Grid>
+      </Container>
+    </ThemeProvider>
+  );
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { CssBaseline } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
@@ -6,7 +5,6 @@ import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <CssBaseline />
     <App />
   </React.StrictMode>
 );
